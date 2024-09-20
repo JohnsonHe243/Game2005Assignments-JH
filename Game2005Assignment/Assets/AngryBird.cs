cf0cf67b378fcfb89d3e7dc30e732c4d828e1a5e
JohnsonHe243 <156115685+JohnsonHe243@users.noreply.github.com>
2024-09-20 19:22:26 -0400

    Finished AngryBird Script.
    Now velocity is adjustable by changing Speed and Angle Degrees in the inspector.

--- Game2005Assignment/Assets/AngryBird.cs
+++ Game2005Assignment/Assets/AngryBird.cs
@@ -2,9 +2,13 @@ using UnityEngine;
 
 public class AngryBird : MonoBehaviour
 {
-    public float angleDegrees = 30;
-    public float speed = 100;
+    public float angleDegrees = 30f;
+    public float speed = 1f;
     public float startHeight = 1;
+
+    public float angleRadiant;
+    public float x;
+    public float y;
 
     public float deltaTime = 0.02f;
 
@@ -12,10 +16,30 @@ public class AngryBird : MonoBehaviour
     public Vector3 gravityAcceleration = new Vector3(0, -10, 0);
     void Update()
     {
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    Debug.Log("Launch!");
+
+        //    //velocity = new Vector3(10, 16);
+        //    x = (Mathf.Cos(angleDegrees) * speed);
+        //    //y = (float)(speed * Mathf.Sin(angleDegrees));
+        //    y = Mathf.Sqrt(Mathf.Pow(speed, 2) - Mathf.Pow(x, 2));
+        //    velocity = new Vector3(x, y);
+
+        //    transform.position = new Vector3(0, startHeight, 0);
+        //    Debug.DrawLine(transform.position, transform.position + velocity, Color.red, 2);
+        //}
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Debug.Log("Launch!");
-            velocity = new Vector3(10, 16);
+
+            angleRadiant = angleDegrees * Mathf.Deg2Rad;
+            x = speed * Mathf.Cos(angleRadiant);
+            y = speed * Mathf.Sin(angleRadiant);
+
+            velocity = new Vector3(x, y);
+
             transform.position = new Vector3(0, startHeight, 0);
             Debug.DrawLine(transform.position, transform.position + velocity, Color.red, 2);
         }
@@ -23,6 +47,8 @@ public class AngryBird : MonoBehaviour
 
     private void FixedUpdate()
     {
+
+
         Vector3 prevPos = transform.position;
 
         transform.position = transform.position + velocity * deltaTime;
@@ -30,5 +56,7 @@ public class AngryBird : MonoBehaviour
         Debug.DrawLine(prevPos, transform.position, Color.green, 10);
 
         velocity = velocity + gravityAcceleration * deltaTime;
+
+        Debug.DrawLine(transform.position, transform.position + velocity, Color.red);
     }
 }
