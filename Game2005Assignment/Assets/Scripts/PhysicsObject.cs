0dc88a34e63b8729eaf0639a82417c4e0bb208dd
JohnsonHe243 <156115685+JohnsonHe243@users.noreply.github.com>
2024-10-07 02:03:18 -0400

    Update PhysicsObject.cs
    drag variable

--- Game2005Assignment/Assets/Scripts/PhysicsObject.cs
+++ Game2005Assignment/Assets/Scripts/PhysicsObject.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PhysicsObject : MonoBehaviour
 {
     public float mass = 1.0f;
-    public float drag = 0.1f;
+    public float drag = 0.9f;
     public Vector3 velocity = Vector3.zero;
 
     // Start is called before the first frame update
