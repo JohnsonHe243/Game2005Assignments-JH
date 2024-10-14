3b69439219d94d2da27d51e51ab4dff1c4a0d0c3
JohnsonHe243 <156115685+JohnsonHe243@users.noreply.github.com>
2024-10-14 14:43:42 -0400

    Check collision between sphere finished.

--- Game2005Assignment/Assets/Scripts/PhysicsEngine.cs
+++ Game2005Assignment/Assets/Scripts/PhysicsEngine.cs
@@ -47,27 +47,41 @@ public class PhysicsEngine : MonoBehaviour
             Debug.DrawLine(prevPos, newPos, Color.green, 10);
             Debug.DrawLine(objectA.transform.position, objectA.transform.position + objectA.velocity, Color.red);
         }
-        foreach(PhysicsObject objectA in objects)
+        foreach(PhysicsObject obj in objects)
         {
-            foreach(PhysicsObject objectB in objects)
+            obj.GetComponent<Renderer>().material.color = Color.white;
+        }
+        //Collision check loop
+        for (int i = 0; i < objects.Count; i++) // N
+        {
+            PhysicsObject objectA = objects[i];
+            for (int j = i  + 1; j < objects.Count; j++) // N
             {
+                PhysicsObject objectB = objects[j];
+                if (objectA == objectB) continue;
                 //Check for collisions between objects
-                if(IsOverlappingSpheres(objectA, objectB))
+                if (IsOverlappingSpheres(objectA, objectB))
                 {
                     //colliding
-                    Debug.DrawLine(objectA.transform.position, objectB.transform.position, Color.green);
+                    Debug.DrawLine(objectA.transform.position, objectB.transform.position, Color.red);
+                    //objectA.velocity = -objectA.velocity;
+                    //objectB.velocity = -objectB.velocity;
+                    //color change
+                    objectA.GetComponent<Renderer>().material.color= Color.red;
+                    objectB.GetComponent<Renderer>().material.color = Color.red;
                 }
                 else
                 {
                     //no collisions
                 }
             }
-
         }
     }
     public bool IsOverlappingSpheres(PhysicsObject objectA, PhysicsObject objectB)
     {
         Debug.Log("checking collision between: " + objectA.name + " and " + objectB.name);
-        return false;
+        Vector3 d = objectA.transform.position - objectB.transform.position;
+        float distance = d.magnitude;
+        return distance < objectA.radius + objectB.radius;
     }
 }
